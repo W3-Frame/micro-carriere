fd883fafa48fb0a541986a607698b6b4b082837f
Hermann EHO <hermannleboss@gmail.com>
2024-06-17 11:56:56 +0000

    feat: ypdate cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    },    
+    baseUrl: 'http://localhost:3000/',
   },
 });
