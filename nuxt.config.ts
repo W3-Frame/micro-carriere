7cc55e00bda363385bd61ff1f64d1395f6859bc3
Hermann EHO <hermannleboss@gmail.com>
2024-06-30 16:03:40 +0000

    feat: firebase api for nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: [ "@vueuse/nuxt", "@nuxt/ui"],
   ui: {
     icons: ["heroicons", "simple-icons"],
+  },
+  runtimeConfig: {
+    // Will be available in both client and server
+    firebase: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+    },
+    public: {
+      firebase: {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DA,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+      },
+    },
   },
 });
